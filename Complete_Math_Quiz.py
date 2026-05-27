064cbe81a3dd90afb2bd3537a60db0e60cdd7b4b
MuskyYoungblood <elisalas4380@masseyhigh.school.nz>
2026-05-27 14:33:51 +1200

    Ultimate Math Quiz

--- Complete_Math_Quiz.py
+++ Complete_Math_Quiz.py
@@ -1,11 +1,15 @@
 import random
 import math
 
+# Establishing variables
 pi = math.pi
 rounded_percentage = []
+questions_answered = 0
+quiz_history = []
+correct_ans = []
 
-# Ultimate string checker. Can display any question with any given options.
-def response_check(question, options):
+def ult_str_check(question, options):
+   """Ultimate string checker. Can display any question with any given options"""
    error = f"Please choose from the following: {options}"
    while True:
        response = input(question).lower()
@@ -14,9 +18,8 @@ def response_check(question, options):
                return opt
        print(error)
 
-
-# Classic integer checker
 def int_check(question, low=0):
+   """Classic integer checker"""
    while True:
        try:
            response = int(input(question))
@@ -26,26 +29,24 @@ def int_check(question, low=0):
        except ValueError:
            print("Please enter a valid integer.")
 
-# Checks if user answers are correct or incorrect
 def question_check(answer):
+   """Checks user's questions in comparison to the correct answer"""
    while True:
        try:
            user = float(input(question))
            answer = round(answer, 2)
+           # If the user got the question correct or incorrect, it adds your feedback to the quiz history
+           # History_feedback is what's displayed when user asks for quiz history
            if user == answer:
                feedback = "You got the answer correct."
                history_feedback = f"Question {questions_answered} of {num_questions}: {feedback}"
                quiz_history.append(history_feedback)
                correct_ans.append(1)
-
-
-
                return
            else:
                feedback = "You got the answer incorrect."
                history_feedback = f"Question {questions_answered} of {num_questions}: {feedback}"
                quiz_history.append(history_feedback)
-
                return
 
 
@@ -53,27 +54,37 @@ def question_check(answer):
            print("Please enter a number.")
 
 def instruction():
+    """Gives instructions"""
+
     print('''
 
 **** Instructions ****
 
-Answer area and perimeter math questions according to the difficulty you set.
+Answer area and perimeter math questions according to the difficulty you set: "easy", "medium", or "hard".
 
     ''')
 
 # Main Routine
-questions_answered = 0
-quiz_history = []
-correct_ans = []
-
 print("Welcome to my math quiz!")
 
-if response_check("\nDo you want to see the instructions? ", ["yes", "no"]) == "yes":
+# Asks user for instructions (options are 'yes' and 'no')
+if ult_str_check("\nDo you want to see the instructions? ", ["yes", "no"]) == "yes":
     instruction()
 
-difficulty = response_check("Select a difficulty: ", ["easy", "medium", "hard"])
+# Asks users for difficulty (options are 'easy', 'medium', 'hard')
+difficulty = ult_str_check("Select a difficulty: ", ["easy", "medium", "hard"])
 
+# Asks users for questions
 num_questions = int_check("How many questions would you like? ")
+
+# Colour of mode depends on difficulty
+difficulty_title = difficulty.title()
+if difficulty_title == "Hard":
+    print(f"\n\033[31m**** {difficulty.title()} Mode ****\033[0m")
+elif difficulty_title == "Medium":
+    print(f"\n\033[35m**** {difficulty.title()} Mode ****\033[0m")
+elif difficulty_title == "Easy":
+    print(f"\n\033[32m**** {difficulty.title()} Mode ****\033[0m")
 
 # Game loop starts here
 while num_questions > questions_answered:
@@ -98,15 +109,13 @@ while num_questions > questions_answered:
         if shape == "triangle":
             answer = random_num_a * random_num_b / 2
             question = f"A triangle has a base of {random_num_a}{units} and a height of {random_num_b}. What is its area? "
-
-        if shape == "square":
+        elif shape == "square":
             answer = random_num_a * random_num_a
             question = f"A square has a base of {random_num_a}{units}. What is its area? "
-
-        if shape == "rectangle":
+        elif shape == "rectangle":
             answer = random_num_a * random_num_b
             question = f"A rectangle has a base of {random_num_a}{units} and a height of {random_num_b}{units}. What is its area? "
-        if shape == "circle":
+        elif shape == "circle":
             answer = pi * (random_num_a * random_num_a)
             question = f"A circle has a radius of {random_num_a}{units}. What is its area? "
 
@@ -122,7 +131,7 @@ while num_questions > questions_answered:
         elif shape == "rectangle":
             answer = random_num_a * 2 + random_num_b * 2
             question = f"A rectangle has sides of {random_num_a}{units} and {random_num_b}{units}. What is its perimeter? "
-        else:
+        elif shape == "circle":
             answer = 2 * pi * random_num_a
             question = f"A circle has a radius of {random_num_a}{units}. What is its circumference? "
 
@@ -130,14 +139,17 @@ while num_questions > questions_answered:
     questions_answered += 1
     question_check(answer)
 
-see_history = response_check("\nDo you want to see your game history? ", ["yes", "no"])
+# Asks user for quiz history (options 'yes' and 'no')
+see_history = ult_str_check("\nDo you want to see your game history? ", ["yes", "no"])
+
+# Displays quiz history
 if see_history == "yes":
     print("\nQuiz History:\n")
     for item in quiz_history:
         print(item)
 
+    # Displays user's percentage
     amount_of_correct = len(correct_ans)
-
     percentage = round((amount_of_correct / num_questions) * 100, 2)
     print(f"{percentage}% / 100%")
 
